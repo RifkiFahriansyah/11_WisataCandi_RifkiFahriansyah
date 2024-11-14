6e4d30d69039ac1953807e0a8f6326e7aad05b0c
RifkiFahriansyah <rifkifahriansyah_2327250093@mhs.mdp.ac.id>
2024-11-14 12:42:32 +0700

    Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi_fahri/data/candi_data.dart';
 import 'package:wisata_candi_fahri/screens/detail_screen.dart';
+import 'package:wisata_candi_fahri/screens/favorite_screen.dart';
+import 'package:wisata_candi_fahri/screens/home_screen.dart';
 import 'package:wisata_candi_fahri/screens/profile_screen.dart';
 import 'package:wisata_candi_fahri/screens/search_screens.dart';
 import 'package:wisata_candi_fahri/screens/sign_in_screen.dart';
@@ -18,7 +20,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: "Wisata Candi",
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
+        appBarTheme: const AppBarTheme(
           iconTheme: IconThemeData(color: Colors.deepPurple),
           titleTextStyle: TextStyle(
             color: Colors.deepPurple,
@@ -33,11 +35,64 @@ class MainApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      //Home : DetailScreen(candi: candiList[0]),
-      //Home : const ProfileScreen(),
-      // home: SignInScreen(),
-      // home: SignUpScreen(),
-      home: SearchScreens(),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  // TODO 1. Deklarasikan Variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreens(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO 2. Buat properti body berupa widget yg ditampilkan
+      body: _children[_currentIndex],
+      // TODO 3. Buat properti bottomNavigationBar dengan nilai Theme
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.deepPurple[50],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex, onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.deepPurple),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.deepPurple),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite, color: Colors.deepPurple),
+              label: "Favorite",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.deepPurple),
+              label: "Profile",
+            ),
+          ],
+        ),
+      ),
+      // TODO 4. Buat data dan child dari Theme
     );
   }
 }
---
+++ lib/widgets/item_card.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi_fahri/models/candi.dart';
+
+class ItemCard extends StatefulWidget {
+  //TODO 1. Deklarasikan variabel yg dibutuhkan dan pasang pada konstruktor
+  final Candi candi;
+  const ItemCard({super.key, required this.candi});
+
+  @override
+  State<ItemCard> createState() => _ItemCardState();
+}
+
+class _ItemCardState extends State<ItemCard> {
+  @override
+  Widget build(BuildContext context) {
+    //TODO 6. Implementasi Routing ke DetailScreen
+    return Card(
+      //TODO 2. Tetapkan parameter shape, margin, dan elevation dari cari
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15)
+      ),
+      margin: EdgeInsets.all(4),
+      elevation: 1,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //TODO 3. Buat image sebagai anak dari column
+          Expanded(
+            // TODO 7. Implementasi Hero Animation
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                widget.candi.imageAsset,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                ),
+            ),
+          ),
+          //TODO 4. Buat text sebagai anak dari column
+          Padding(
+            padding: const EdgeInsets.only(left: 16, top: 8),
+            child: Text(
+              widget.candi.name,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          //TODO 5. Buat text sebagai anak dari column
+          Padding(
+            padding: const EdgeInsets.only(left: 16, top: 8),
+            child: Text(
+              widget.candi.type,
+              style: const TextStyle(
+                fontSize: 12
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
