8866d0e743cfb586c9ea316da6fe7c98c7968570
RifkiFahriansyah <rifkifahriansyah_2327250093@mhs.mdp.ac.id>
2024-11-14 13:05:53 +0700

    Inkwell dan Ontap

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:wisata_candi_fahri/models/candi.dart';
@@ -36,7 +35,9 @@ class DetailScreen extends StatelessWidget {
                       shape: BoxShape.circle,
                     ),
                     child: IconButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
                       icon: const Icon(Icons.arrow_back),
                     ),
                   ),
@@ -190,24 +191,23 @@ class DetailScreen extends StatelessWidget {
                                       ),
                                     ),
                                     child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: CachedNetworkImage(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: CachedNetworkImage(
                                           imageUrl: candi.imageUrls[index],
                                           width: 120,
                                           height: 120,
-                                          fit : BoxFit.cover,
-                                          placeholder: (context, 
-                                          url) => Container(
+                                          fit: BoxFit.cover,
+                                          placeholder: (context, url) =>
+                                              Container(
                                             width: 120,
                                             height: 120,
                                             color: Colors.deepPurple[50],
                                           ),
-                                          errorWidget: (context, url, error) => 
-                                          const Icon(
+                                          errorWidget: (context, url, error) =>
+                                              const Icon(
                                             Icons.error,
                                           ),
-                                      )
-                                    ),
+                                        )),
                                   ),
                                 ),
                               );
--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi_fahri/models/candi.dart';
+import 'package:wisata_candi_fahri/screens/detail_screen.dart';
 
 class ItemCard extends StatefulWidget {
   //TODO 1. Deklarasikan variabel yg dibutuhkan dan pasang pada konstruktor
@@ -14,50 +15,55 @@ class _ItemCardState extends State<ItemCard> {
   @override
   Widget build(BuildContext context) {
     //TODO 6. Implementasi Routing ke DetailScreen
-    return Card(
-      //TODO 2. Tetapkan parameter shape, margin, dan elevation dari cari
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15)
-      ),
-      margin: EdgeInsets.all(4),
-      elevation: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //TODO 3. Buat image sebagai anak dari column
-          Expanded(
-            // TODO 7. Implementasi Hero Animation
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.asset(
-                widget.candi.imageAsset,
-                width: double.infinity,
-                fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder:(context) => DetailScreen(candi: widget.candi)));
+      },
+      child: Card(
+        //TODO 2. Tetapkan parameter shape, margin, dan elevation dari cari
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)
+        ),
+        margin: EdgeInsets.all(4),
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //TODO 3. Buat image sebagai anak dari column
+            Expanded(
+              // TODO 7. Implementasi Hero Animation
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(
+                  widget.candi.imageAsset,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  ),
+              ),
+            ),
+            //TODO 4. Buat text sebagai anak dari column
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                widget.candi.name,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-            ),
-          ),
-          //TODO 4. Buat text sebagai anak dari column
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              widget.candi.name,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          //TODO 5. Buat text sebagai anak dari column
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              widget.candi.type,
-              style: const TextStyle(
-                fontSize: 12
+            //TODO 5. Buat text sebagai anak dari column
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                widget.candi.type,
+                style: const TextStyle(
+                  fontSize: 12
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
